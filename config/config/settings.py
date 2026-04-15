9a85457786e3e70b060856a02339bb6ca8446ee2
dohwi <dohwe0978@gmail.com>
2026-04-15 15:12:47 +0900

    ALLOWED_HOSTS 추가

--- config/config/settings.py
+++ config/config/settings.py
@@ -28,7 +28,10 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "52.79.105.138",
+    "localhost",
+]
 
 
 # Application definition
